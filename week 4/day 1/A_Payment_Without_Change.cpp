5228b4d99e04c7b3497a5bcd185f454a8fdc298e
NI Maruf <nimaruf04@gmail.com>
2024-11-20 23:15:30 +0600

    Add files via upload

---
+++ week 4/day 1/A_Payment_Without_Change.cpp
@@ -0,0 +1,46 @@
+// اعوذ بالله من الشيطان الرجيم
+//  'بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ
+ 
+// Date:   2024.11.20
+// Time:   23:02:44
+#include <bits/stdc++.h>
+using namespace std; 
+#define int long long
+#define float double
+#define ccase cout<<"Case"<<" "<<T<<" "<<':'<<" "
+#define cyes cout<<"YES"<<endl
+#define cno cout<<"NO"<<endl
+#define endl '\n'
+#define NI                       \
+    ios::sync_with_stdio(false);  \
+    cin.tie(NULL);                 \
+    cout.tie(NULL);                 \
+ 
+#define MARUF                  \
+    int TC;                     \
+    cin >> TC;                   \
+    for (int T = 1; T <= TC; T++) 
+void solve();
+int32_t main()
+{
+    NI MARUF
+    {
+        solve();
+    }
+    return 0;
+}
+void solve()
+{
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    int x=a*c;
+      if(x<=d){
+        if(x+b>=d)cyes;
+        else cno;
+      }
+      else {
+         int r=d%c;
+         if(r<=b)cyes;
+         else cno;
+      }
+}
